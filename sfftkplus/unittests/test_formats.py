34e5f1a7cfa76857941d8cf4203cb031872c730f
Paul K. Korir, PhD <pkorir@ebi.ac.uk>
2018-07-16 11:45:10 +0100

    Code cleanup

--- sfftkplus/unittests/test_formats.py
+++ sfftkplus/unittests/test_formats.py
@@ -5,17 +5,16 @@ sfftkplus.formats modules unit tests
 """
 from __future__ import division
 
-__author__  = "Paul K. Korir, PhD"
-__email__   = "pkorir@ebi.ac.uk, paul.korir@gmail.com"
-__date__    = "2017-08-14"
-
+__author__ = "Paul K. Korir, PhD"
+__email__ = "pkorir@ebi.ac.uk, paul.korir@gmail.com"
+__date__ = "2017-08-14"
 
 import os
 import shlex
 import unittest
 import __init__ as tests
-from sfftkplus.core.parser import parse_args
-from sfftkplus.formats import roi
+from ..core.parser import parse_args
+from ..formats import roi
 from ..schema import SFFPSegmentation
 
 
@@ -26,19 +25,20 @@ class TestFormats(unittest.TestCase):
         cls.sff_file = os.path.join(tests.TEST_DATA_PATH, 'sff', 'test_emd_1832.sff')
         cls.roi_segmentation = roi.ROISegmentation(cls.roi_file)
         cls.sff_segmentation = SFFPSegmentation(cls.sff_file)
-    
+
     def test_roi_read(self):
         """Test that we can read .roi files"""
         self.assertIsInstance(self.roi_segmentation.header, roi.ROIHeader)
         self.assertEqual(len(self.roi_segmentation.segments), 6)
-    
+
     def test_from_vtk(self):
         """Test that we can create an ROISegmentation from VTK"""
-        args, configs = parse_args(shlex.split('createroi {} -o file.roi'.format(os.path.join(tests.TEST_DATA_PATH, 'test_emd_1832.sff'))))
+        args, configs = parse_args(
+            shlex.split(
+                'createroi {} -o file.roi'.format(os.path.join(tests.TEST_DATA_PATH, 'sff', 'test_emd_1832.sff'))))
         vtk_segmentation = self.sff_segmentation.as_vtk(args, configs)
-        # to get contours we must first slice!
+        #  to get contours we must first slice!
         vtk_segmentation.slice()
-        roi_segmentation = vtk_segmentation.as_roi(configs)
+        roi_segmentation = vtk_segmentation.as_roi(args, configs)
         self.assertEqual(len(roi_segmentation.segments), len(self.roi_segmentation.segments))
         self.assertIsInstance(roi_segmentation.segments[0], type(self.roi_segmentation.segments[0]))
-        
